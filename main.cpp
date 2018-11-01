4e68aa64a95ed8ebe7cfe4f3ff5203416ec5d13e
Danilo Silva <danilojose26@hotmail.com>
2018-11-01 13:19:05 -0300

    introduzindo main2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <stdio.h>
 
 #include "tabuleiro.h"
 
@@ -7,22 +8,77 @@ using namespace std;
 
 int main() {
 
-	Tabuleiro t;
+	Tabuleiro t;		
 
-	while(true) {
+	while(true)
+	{
 
 		for(int i = 0 ; i < 8 ; i++) {
 
 			for(int j = 0; j < 8 ; j++) {
 
-				std::cout.width(3);
-				std::cout << t.getMatriz(i, j) << ' ';
+				string nome_peca = t.getMatriz(i, j);
+				char tipo = nome_peca[0];
+				char cor = nome_peca[nome_peca.size() - 1];
+				int numero = nome_peca[1] - '1';
 
+				std::cout.width(4);				
+
+				if( cor == 'P')
+				{
+					switch(tipo)
+					{
+
+						case 'T':				
+							std::cout << " \u265C" << ' ';
+							break;
+						case 'C':				
+							std::cout << " \u265E" << ' ';
+							break;
+						case 'B':				
+							std::cout << " \u265D" << ' ';
+							break;					
+						case 'P':				
+							std::cout << " \u265F" << ' ';
+							break;				
+						case 'R':				
+							std::cout << " \u265A" << ' ';
+							break;					
+						case 'D':				
+							std::cout << " \u265B" << ' ';
+							break;								
+					}			
+				}
+
+				else if( cor == 'B')
+				{
+					switch(tipo)
+					{
+
+						case 'T':				
+							std::cout << " \u2656" << ' ';
+							break;
+						case 'C':				
+							std::cout << " \u2658" << ' ';
+							break;
+						case 'B':				
+							std::cout << " \u2657" << ' ';
+							break;					
+						case 'P':				
+							std::cout << " \u2659" << ' ';
+							break;					
+						case 'R':				
+							std::cout << " \u2654" << ' ';
+							break;					
+						case 'D':				
+							std::cout << " \u2655" << ' ';
+							break;								
+					}			
+				}							
 			}
-
+			
 			std::cout << std::endl;
-		}
-		
+		}	
 
 		std::cout << "Entrada: ";
 		int x, y;
